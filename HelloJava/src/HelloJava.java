54b6547a0c6e1ccb253349345d446b435f3b530c
Haneul Kim <dylan1104@naver.com>
2021-11-01 11:17:01 +0900

    conflict complete

--- HelloJava/src/HelloJava.java
+++ HelloJava/src/HelloJava.java
@@ -5,13 +5,9 @@ public class HelloJava {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 		System.out.println("World's End Dancehall");
-<<<<<<< HEAD
 		System.out.println("test1");
-=======
 		System.out.println("test2");
-		
 
->>>>>>> refs/heads/hotfix
 	}
 
 }
