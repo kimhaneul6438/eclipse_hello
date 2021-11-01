14f789a0d472b2d6ca83eaf9fdf0fdd2e3618dd4
Haneul Kim <dylan1104@naver.com>
2021-11-01 11:00:58 +0900

    added print line

--- HelloJava/src/HelloJava.java
+++ HelloJava/src/HelloJava.java
@@ -3,8 +3,8 @@ public class HelloJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hellojava3");
-		System.out.println("Hellojava2");
+		System.out.println("Hello World");
+		System.out.println("World's End Dancehall");
 		
 		
 
